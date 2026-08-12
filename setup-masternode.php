c3d8ec02e37384f04ab730eb4115a3d8c068e5b3
Dattatray <dattatrayh@gmail.com>
2026-08-12 12:25:50 +0530

    Update index.php
    Update events.php, Update setup-masternode.php, Update header.php

--- setup-masternode.php
+++ setup-masternode.php
@@ -214,7 +214,7 @@
                 <div class="card h-100 overflow-hidden pt-4 pb-4 px-3 bg-light75 br-20 border-0">
                     <div class="card-info text-center w-100">
                         <p class="fw-500 fs-6">Total XDC Locked</p>
-                        <div class="fw-600 lh-1 fs-2 mb-3"><span class="counter-api counter" id="tvl">3200.87</span> <span id="tvltext">M</span></div>
+                        <div class="fw-600 lh-1 fs-2 mb-3"><span class="counter-api counter" id="tvl">3030.87</span> <span id="tvltext">M</span></div>
                         <p class="fw-500 fs-7 mb-0">XDC</p>
                     </div>
                 </div>
